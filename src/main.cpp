26eba68d4003ccdd8a589c5944ed04bb80a25687
onqtam <vik.kirilov@gmail.com>
2018-02-12 00:23:01 +0200

    trying to fix the retina issue by editing a bit the imgui glfw setup code - made the result from the window size and framebuffer size the same

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,6 @@ int main() {
     if(!glfwInit())
         return 1;
 
-	// use full resolution framebuffers on Retina displays - MacOS only
-	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_TRUE);
-
     GLFWwindow* window = glfwCreateWindow(1280, 1024, "Read-Compile-Run-Loop - REPL for C++", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
